6665374cb6f06fbd3555850da4114e18f650b2b6
GabrielMeneze <65770195+GabrielMeneze@users.noreply.github.com>
2020-11-26 22:50:18 -0300

    DDL 90%

--- DDL semi-pronta.sql
+++ DDL semi-pronta.sql
@@ -1,32 +1,62 @@
 create database PixFourFun;
-
+USE PixFourFun;
 CREATE TABLE Usuario(
-	IdUsuario INT NOT NULL PRIMARY KEY AUTO_INCREMENT
-);
+	IdUsuario INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    
+    
+    Nome VARCHAR(100),
+    Email VARCHAR(40),
+    Senha VARCHAR(50),
+    Telefone VARCHAR(30)
+    );
 
 CREATE TABLE Cupom(
-	IdCupom INT NOT NULL PRIMARY KEY AUTO_INCREMENT
+	IdCupom INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    
+    
+    ValorDesconto FLOAT,
+    DataValidade DATETIME,
+    PalavraChave VARCHAR(100)
 );
 
 CREATE TABLE Fotos(
-	IdFotos INT NOT NULL PRIMARY KEY AUTO_INCREMENT
+	IdFotos INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    
+    
+    FraseFoto VARCHAR(60)
 );
 
 CREATE TABLE Pack(
 	IdPack  INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    FOREIGN KEY (IdFotos) REFERENCES Fotos(IdFotos)
+    IdFotos INT,
+    FOREIGN KEY (IdFotos) REFERENCES Fotos(IdFotos),
+    
+    
+    TipoPack VARCHAR(15),
+    Preco FLOAT
 );
 
 CREATE TABLE Pagamento(
 	IdPagamento INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    FOREIGN KEY (IdUsuarios) REFERENCES Usuarios(IdUsuarios)
+    IdUsuario int,
+    FOREIGN KEY (IdUsuario) REFERENCES Usuario(IdUsuario),
+    
+    TipoPgto VARCHAR(50),
+    ValorTotal FLOAT,
+    TipoEnvio VARCHAR(80),
+    StatusPgto VARCHAR(100)
 );
 
 CREATE TABLE Pedido(
 	IdPedido INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
-    FOREIGN KEY (IdUsuarios) REFERENCES Usuarios(IdUsuarios),
+    IdUsuario int,
+    FOREIGN KEY (IdUsuario) REFERENCES Usuario(IdUsuario),
+    IdPack int,
     FOREIGN KEY (IdPack) REFERENCES Pack(IdPack),
-	FOREIGN KEY (IdCupom) REFERENCES Cupom(IdCupom)
+    IdCupom int,
+	FOREIGN KEY (IdCupom) REFERENCES Cupom(IdCupom),
+    IdPagamento int,
+    FOREIGN KEY (IdPagamento) REFERENCES Pagamento(IdPagamento)
     
 );
 
